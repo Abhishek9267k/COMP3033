a02553302a313023a33bb9eaf90c7a26c941189c
Abhishek9267k <91148557+Abhishek9267k@users.noreply.github.com>
2021-10-13 23:37:54 -0400

    Lab 2

---
+++ routes/lab_2.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.get('/', (req, res, next) => {
+
+    var num1 = req.query.x;                                         // storing temeperaory value of variable x
+    var num2 = req.query.y;                                         // storing temeperaory value of variable y
+    var h = parseInt(num1);                                     // parsing value of h in variable h
+    var j = parseInt(num2);                                     // parsing value of h in variable j
+    var method = req.query.method;                                 // parameter to request the type of method
+    var result;                                                      // temporary variable to store result
+
+    if(req.query.method == 'add'){
+        result = h+j;
+    }
+    else if(req.query.method == 'multiply'){
+        result = h*j;
+    }
+    else if(req.query.method == 'subtract'){
+        result = h-j;
+    }
+    else if(req.query.method == 'divide'){
+        result = h/j;
+    }
+    else {
+        result = "Error, Please enter a proper method";
+    }
+    res.json({
+        "x" : h,
+        "y" : j,
+        "operation" : method,
+        "result" : result
+
+        
+    });
+});
+
+module.exports = router;
