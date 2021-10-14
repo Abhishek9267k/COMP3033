fb76afbf36bccb20b936de4f91476b6732026579
Abhishek9267k <91148557+Abhishek9267k@users.noreply.github.com>
2021-10-13 23:42:14 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express');                                          // calling express function
+var router = express.Router();                                            // handling all the routings
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
