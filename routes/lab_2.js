22dd71b37f5c7ea3d5d9ec04d05b12d67f10f2ba
Abhishek9267k <91148557+Abhishek9267k@users.noreply.github.com>
2021-10-13 23:50:19 -0400

    Update lab_2.js

--- routes/lab_2.js
+++ routes/lab_2.js
@@ -11,19 +11,19 @@ router.get('/', (req, res, next) => {
     var method = req.query.method;                                 // parameter to request the type of method
     var result;                                                      // temporary variable to store result
 
-    if(req.query.method == 'add'){
+    if(req.query.method == 'add'){ //addition function
         result = h+j;
     }
-    else if(req.query.method == 'multiply'){
+    else if(req.query.method == 'multiply'){ //multiply function
         result = h*j;
     }
-    else if(req.query.method == 'subtract'){
+    else if(req.query.method == 'subtract'){ //subtraction function
         result = h-j;
     }
-    else if(req.query.method == 'divide'){
+    else if(req.query.method == 'divide'){ //division function
         result = h/j;
     }
-    else {
+    else { // not any method
         result = "Error, Please enter a proper method";
     }
     res.json({
@@ -35,5 +35,5 @@ router.get('/', (req, res, next) => {
         
     });
 });
-
+////routers
 module.exports = router;
